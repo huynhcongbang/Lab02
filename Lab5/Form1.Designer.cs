fd0f572b2adb3dfe07ff651654a162e1f837443d
Admin <Admin@ADMIN>
2024-06-05 08:21:20 +0700

    Add project files.

---
+++ Lab5/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBai1 = new System.Windows.Forms.Button();
+            this.btnBai2 = new System.Windows.Forms.Button();
+            this.btnBai4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBai1
+            // 
+            this.btnBai1.Location = new System.Drawing.Point(58, 72);
+            this.btnBai1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBai1.Name = "btnBai1";
+            this.btnBai1.Size = new System.Drawing.Size(116, 44);
+            this.btnBai1.TabIndex = 0;
+            this.btnBai1.Text = "Bài 1";
+            this.btnBai1.UseVisualStyleBackColor = true;
+            this.btnBai1.Click += new System.EventHandler(this.btnBai1_Click);
+            // 
+            // btnBai2
+            // 
+            this.btnBai2.Location = new System.Drawing.Point(198, 72);
+            this.btnBai2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBai2.Name = "btnBai2";
+            this.btnBai2.Size = new System.Drawing.Size(116, 44);
+            this.btnBai2.TabIndex = 0;
+            this.btnBai2.Text = "Bài 2 + 3 ";
+            this.btnBai2.UseVisualStyleBackColor = true;
+            this.btnBai2.Click += new System.EventHandler(this.btnBai2_Click);
+            // 
+            // btnBai4
+            // 
+            this.btnBai4.Location = new System.Drawing.Point(350, 72);
+            this.btnBai4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBai4.Name = "btnBai4";
+            this.btnBai4.Size = new System.Drawing.Size(91, 42);
+            this.btnBai4.TabIndex = 1;
+            this.btnBai4.Text = "Bài 6";
+            this.btnBai4.UseVisualStyleBackColor = true;
+            this.btnBai4.Click += new System.EventHandler(this.btnBai4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnBai4);
+            this.Controls.Add(this.btnBai2);
+            this.Controls.Add(this.btnBai1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBai1;
+        private System.Windows.Forms.Button btnBai2;
+        private System.Windows.Forms.Button btnBai4;
+    }
+}
+
